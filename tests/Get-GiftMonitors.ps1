f9d9014c6800ca0ca99dbfe9f266b2325ddd1305
Jim Benjamin <jrbenj@gmail.com>
2025-06-20 13:57:45 -0400

    Update output messages in Get-GiftMonitors and Get-GiftVideo functions for clarity

--- tests/Get-GiftMonitors.ps1
+++ tests/Get-GiftMonitors.ps1
@@ -3,7 +3,7 @@ function Get-GiftMonitors {
         [string]$ApiUrl = "http://127.0.0.1:8000/list_monitors"
     )
     $response = Invoke-RestMethod -Uri $ApiUrl -Method Get
-    Write-Host "Available TikFinity Monitors:`n"
+    Write-Host "Available VLC Monitors:`n"
     foreach ($monitor in $response) {
         Write-Host ("- {0}: {1}" -f $monitor.monitor_key, $monitor.device_name)
     }
--- tests/Get-GiftVideos.ps1
+++ tests/Get-GiftVideos.ps1
@@ -3,7 +3,7 @@ function Get-GiftVideo {
         [string]$ApiUrl = "http://127.0.0.1:8000/list_videos"
     )
     $response = Invoke-RestMethod -Uri $ApiUrl -Method Get
-    Write-Host "Available TikFinity Videos:`n"
+    Write-Host "Available Videos:`n"
     foreach ($video in $response) {
         Write-Host ("- {0}: {1}" -f $video.video_key, $video.file_path)
     }
